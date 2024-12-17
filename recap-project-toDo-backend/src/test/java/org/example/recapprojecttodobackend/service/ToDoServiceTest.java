3a55aaca45cc9564139de5615a221ea7735befa2
Torben Jost <torbenjost@proton.me>
2024-12-17 11:26:28 +0100

    start test getById

--- recap-project-toDo-backend/src/test/java/org/example/recapprojecttodobackend/service/ToDoServiceTest.java
+++ recap-project-toDo-backend/src/test/java/org/example/recapprojecttodobackend/service/ToDoServiceTest.java
@@ -41,7 +41,7 @@ class ToDoServiceTest {
         ToDo toDo2 = new ToDo("2", "Sign up for bootcamp", ToDo.toDoStatus.DONE);
         ToDoDTO toDoDTO1 = new ToDoDTO(null, "Finish bootcamp", ToDo.toDoStatus.TODO);
         ToDoDTO toDoDTO2 = new ToDoDTO(null, "Sign up for bootcamp", ToDo.toDoStatus.DONE);
-        ToDoService toDoService = new ToDoService(toDoRepo, idService);; // the mocked repo again where we will "find" our figure
+        ToDoService toDoService = new ToDoService(toDoRepo, idService);
         List<ToDo> toDoList = List.of(toDo1, toDo2);
         when(toDoRepo.findAll()).thenReturn(toDoList);
         List<ToDoDTO> expected = List.of(toDoDTO1, toDoDTO2);
@@ -50,5 +50,20 @@ class ToDoServiceTest {
         //THEN
         assertEquals(expected, actual);
     }
+
+    // GetById Tests
+//    @Test
+//    void getById_shouldReturnToDoDTO1_whenCalledWithValidId() {
+//        //GIVEN
+//        ToDo toDo1= new ToDo("1", "Finish bootcamp", ToDo.toDoStatus.TODO);
+//        ToDoService toDoService = new ToDoService(toDoRepo, idService);
+//        when(toDoRepo.findById(toDo1.id())).thenReturn(Optional.of(toDo1)); // mocking that the figure is being found from the repo
+//
+//        ToDoDTO expected = new ToDoDTO(null, toDo1.description(), toDo1.toDoStatus());
+//        //WHEN
+//        ToDoDTO actual = figureService.getById(figure.id());
+//        //THEN
+//        assertEquals(expected, actual);
+//    }
 
 }
